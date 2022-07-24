13b719107da330bebde2cf225497691b46721c3b
Anusha260 <anushananabala074@gmail.com>
2022-07-24 23:09:32 +0530

    first commit

---
+++ src/components/Greet.js
@@ -0,0 +1,13 @@
+import React, { component } from 'react';
+import logo from './logo.svg';
+import "./App.css";
+import Greet from './components/Greet'
+class App extends component {
+    render() {
+        return ( 
+            <hii></hii>
+
+        )
+    }
+
+}
